1c2e1397735894cb86f6d00eeb85ef52a4c40eda
Kenad Araujo <kenadwanderson@gmail.com>
2022-02-20 22:48:29 -0300

    - Um teste mal sucedido

--- src/main/java/com/polligonalApps/rabbitmqExample/queue/QueueReceiver.java
+++ src/main/java/com/polligonalApps/rabbitmqExample/queue/QueueReceiver.java
@@ -1,14 +1,24 @@
 package com.polligonalApps.rabbitmqExample.queue;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class QueueReceiver {
+	
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+	
+	@Autowired
+	private Queue queue;
 
 	@RabbitListener(queues = {"${queue.name}"})
 	public void receive(@Payload String uuid) {
 		System.out.println("Mensagem recebida:"+uuid);
 	}
+	
 }
